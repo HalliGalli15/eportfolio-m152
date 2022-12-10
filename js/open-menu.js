55e6a06a126bb836c72d6335358a12c7e648a21c
Damian Hug <damian.hug@outlook.com>
2022-12-10 11:27:55 +0100

    responsive nav

--- js/open-menu.js
+++ js/open-menu.js
@@ -1,11 +1,15 @@
 function openMenu() {
   var x = document.getElementById("nav-items");
   var y = document.getElementById("navbar");
+  var navButton = document.getElementById("nav-button");
+
   if (x.style.display === "block") {
     x.style.display = "none";
-    y.style.height = "100";
+    y.style.height = "150px";
+    navButton.style.transform = "rotate(0)";
   } else {
     x.style.display = "block";
-    y.style.height = "350";
+    y.style.height = "350px";
+    navButton.style.transform = "rotate(180deg)";
   }
 }
