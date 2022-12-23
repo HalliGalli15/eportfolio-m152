f626b786c8d847746873fa2b29610698568e6be9
Damian Hug <damian.hug@gemdat.ch>
2022-12-23 15:54:59 +0100

    rearranged some values

--- js/open-menu.js
+++ js/open-menu.js
@@ -1,13 +1,6 @@
 function openMenu() {
   var items = document.getElementById("nav-items");
-  var navbar = document.getElementById("navbar");
-  var navButton = document.getElementById("nav-button");
 
-  if (items.style.display === "block") {
-    items.style.display = "none";
-    navButton.style.transform = "rotate(0)";
-  } else {
-    items.style.display = "block";
-    navButton.style.transform = "rotate(180deg)";
-  }
+  items.classList.toggle('open');
+  
 }
